2d25de1ff5f6b68a0b1bf1b749fd51d7ac99c4c6
dddqmmx <1538080366@qq.com>
2023-06-07 16:16:21 +0800

    push test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    //fzqsb
+    public static void fzq(){
+        System.out.println("fzqsb");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        fzq();
     }
 
 }
