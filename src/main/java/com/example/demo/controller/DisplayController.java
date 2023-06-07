1565e34a9de1e84e314cb3af194241a8dc8e01d3
ZedFeorius <zedfeorius7879@gmail.com>
2023-06-08 01:26:40 +0800

    已完成：每日一推，对外展示，内部活动，显示内容编辑器
    Co-Authored-By: dddqmmx <58079930+dddqmmx@users.noreply.github.com>

--- src/main/java/com/example/demo/controller/DisplayController.java
+++ src/main/java/com/example/demo/controller/DisplayController.java
@@ -1,12 +1,43 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/display")
 public class DisplayController {
-    @RequestMapping("/push_every_fucking_day")
-    public String display() {
-        return "display";
+    @RequestMapping("/daily_info")
+    public String toDailyInfo() {
+        return "display/daily_info";
+    }
+    @RequestMapping("/inner_activities")
+    public String toInnerActivities() {
+        return "display/inner_activities";
+    }
+
+    @RequestMapping("/external_performance")
+    public String toExternalPerformance() {
+        return "display/external_performance";
+    }
+
+    @RequestMapping("/editor")
+    public String toEditor(
+            @RequestParam(required = false) String type,
+            @RequestParam(required = false) String signupNum,
+            @RequestParam(required = false) String startTime,
+            @RequestParam(required = false) String endTime,
+            Model model){
+        model.addAttribute("type", "".equals(type.trim())?"Display":type);
+        model.addAttribute("signupNum",signupNum==null?"":signupNum);
+        model.addAttribute("startTime",startTime==null?"":startTime);
+        model.addAttribute("endTime",endTime==null?"":endTime);
+        return "display/editor";
+    }
+
+    @RequestMapping("/edit_activities")
+    public String toEditorActivites(){
+        return "display/edit_activities";
     }
 }
