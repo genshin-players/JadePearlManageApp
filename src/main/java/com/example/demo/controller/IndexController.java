f96c026256290e2b3f4d5c8ced1b3af0418e4d0b
XXX9900 <ch3187895550@icloud.com>
2023-06-08 22:29:28 +0800

    新增出勤模块 修改添加工作安排页面

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -29,6 +30,7 @@ public class IndexController {
     public String toIndex(){
         return "redirect:/";
     }
+
 
     @RequestMapping("teacher")
     public String teacher(){
@@ -67,7 +69,7 @@ public class IndexController {
     public String toStuAttendance(){
         return "work/studentAttendance";
     }
-    //学生出勤详情页面请求f
+    //学生出勤详情页面请求
     @RequestMapping("/toStuAttendanceDetail")
     public String toStuAttendanceDetail(String className, String total, String late, String absence, String date, Model model) throws ParseException {
         model.addAttribute("className", className);
@@ -85,5 +87,38 @@ public class IndexController {
     @RequestMapping("/toMembersAttendance")
     public String toMembersAttendance(){
         return "work/MembersAttendance";
+    }
+
+    //学社出勤详情页面请求
+    @RequestMapping("/toMemAttendanceDetail")
+    public String toMemAttendanceDetail(String memName,String date,Model model) throws ParseException {
+        model.addAttribute("memName", memName);
+
+
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        Date date1=sdf.parse(date);
+        model.addAttribute("date", date1);
+        return "work/MembersAttendanceDetail";
+    }
+
+
+
+    //班级出勤编辑页面请求
+    @RequestMapping("/toEditStudentAttendance")
+    public String toEditStudentAttendance(){
+        return "work/editStudentAttendance";
+    }
+
+
+
+    //安排工作页面
+    @RequestMapping("/toAssignWork")
+    public String toAssignOneWork(@RequestParam(required = false) String memName,Model model){
+        if(memName!=null){
+            model.addAttribute("memName", memName);
+        }else {
+            model.addAttribute("memName", "sb");
+        }
+        return "work/assignWork";
     }
 }
