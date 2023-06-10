d7a89d094d569c1281f34edf4aab8960887bdc48
ZedFeorius <zedfeorius7879@gmail.com>
2023-06-10 19:58:44 +0800

    fix

---
+++ display/src/main/java/cn/bdqn/DisplayApp.java
@@ -0,0 +1,21 @@
+package cn.bdqn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author ZedFeorius
+ * @version 1.0.0
+ * @date 06 10 2023  19:55:01
+ * @packageName cn.bdqn
+ * @className DisplayApp
+ * @describe TODO
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class DisplayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DisplayApp.class,args);
+    }
+}
