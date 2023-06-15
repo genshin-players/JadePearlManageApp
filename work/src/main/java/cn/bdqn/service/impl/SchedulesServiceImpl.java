84b4d3e51789c549ac3143cca879e96b69a59113
zhaoguangy <135818288+zhaoguangy@users.noreply.github.com>
2023-06-15 21:54:44 +0800

    Revert "Revert "Revert "Merge remote-tracking branch 'origin/main'"""
    This reverts commit 973874fb39469f46cfb9dcd137c85d53a4c0e5ef.

--- work/src/main/java/cn/bdqn/service/impl/SchedulesServiceImpl.java
+++ work/src/main/java/cn/bdqn/service/impl/SchedulesServiceImpl.java
@@ -34,8 +34,6 @@ public class SchedulesServiceImpl extends ServiceImpl<SchedulesMapper, Schedules
     ClassesMapper classesMapper;
     @Autowired
     ClassMapper classMapper;
-    @Autowired
-    WorkClassMapper workClassMapper;
 
 
     /**
@@ -56,13 +54,6 @@ public class SchedulesServiceImpl extends ServiceImpl<SchedulesMapper, Schedules
             for(Schedules s:schedules){
                 MemberWorkCardInfoVO vo=new MemberWorkCardInfoVO();
                 vo.setSchedules(s);
-
-                //判断工作类型如果为1查班 查询负责班级
-                if(s.getWorkTypeId()==1){
-                    List<WorkClass> workClasses=workClassMapper.selectList(new QueryWrapper<WorkClass>().eq("schedules_id",s.getId()));
-                    vo.setWorkClasses(workClasses);
-                }
-
                 //获取成员
                 Users users= usersMapper.selectById(s.getMemberId());
                 vo.setMember(users);
@@ -85,7 +76,7 @@ public class SchedulesServiceImpl extends ServiceImpl<SchedulesMapper, Schedules
 
 
 
-        return voList;
+        return null;
     }
 
 
@@ -105,14 +96,6 @@ public class SchedulesServiceImpl extends ServiceImpl<SchedulesMapper, Schedules
         if(schedules!=null&&schedules.size()>0){
             for (Schedules s:schedules){
                 MemberWorkDetailInfoVO vo=new MemberWorkDetailInfoVO();
-
-                //判断工作类型如果为1查班 查询负责班级
-                if(s.getWorkTypeId()==1){
-                    List<WorkClass> workClasses=workClassMapper.selectList(new QueryWrapper<WorkClass>().eq("schedules_id",s.getId()));
-                    vo.setWorkClasses(workClasses);
-                }
-
-
                 //获取学社成员
                 Users member=usersMapper.selectById(s.getMemberId());
                 vo.setMember(member);
